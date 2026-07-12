226bd5bd699ddcf4aba3f864dbbf9db135a7423e
Xyra Sinclair <xyra@xyrasinclair.com>
2026-07-12 00:00:59 -0700

    chore: scaffold weak-collections package

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        include: ['src/**/*.test.ts'],
+        pool: 'forks',
+        poolOptions: {
+            forks: {
+                execArgv: ['--expose-gc'],
+            },
+        },
+    },
+})
+
